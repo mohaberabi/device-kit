a8062f1e74e64e4328aa81ce3553d8c7179a1b92
Mohab Erabi <mohaberabi98@icloud.com>
2025-02-12 14:34:04 +0200

    initailized maven artifacts

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,3 +1,4 @@
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
@@ -5,6 +6,8 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    id("com.vanniktech.maven.publish") version "0.30.0"
+
 }
 
 kotlin {
@@ -51,4 +54,40 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+}
+
+mavenPublishing {
+    coordinates(
+        groupId = "io.github.mohaberabi",
+        artifactId = "device-kit",
+        version = "0.0.1"
+    )
+
+    pom {
+        name.set("KMP Library for poc")
+        description.set("This library is just a poc")
+        inceptionYear.set("2025")
+        url.set("https://github.com/mohaberabi/device-kit")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("mohaberabi")
+                name.set("Mohab Erabi")
+                email.set("moohaberabii98@gmail.com")
+            }
+        }
+        scm {
+            url.set("https://github.com/mohaberabi/device-kit")
+        }
+    }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
 }
